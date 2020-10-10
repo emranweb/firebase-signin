89d73179c40a8e242cf936752b74e53138c7471a
emranweb <emranbd77@gmail.com>
2020-10-10 21:41:07 +0600

    user data state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import {Switch, Route} from "react-router-dom";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Home from "./component/Home.js";
 import Header from "./component/Header";
 import SignIn from "./component/SignIn";
 import SignUp from "./component/SignUp";
 
-function App() {
-  return (
-   <div className="main-wrapper">
-     <Header />
-    <Switch>
-      <Route  path="/" exact component={Home}/>
-      <Route path="/signin" component={SignIn} />
-      <Route path="/signup" component={SignUp} />
-    </Switch>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { user: [] };
+  }
 
-   </div>
-  );
+  getUserData = (value) => {
+    this.setState({ user: [value] });
+  };
+
+  render() {
+    return (
+      <div className="main-wrapper">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/signin"
+            render={() => <SignIn userData={this.getUserData} />}
+          />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -2,52 +2,79 @@ import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-function SignIn() {
-  return (
-    <div className="signin-wrapper mt-5">
-      <Container>
-        <Row>
-          <Col md={{ span: 6, offset: 3 }}>
-            <h2 className="text-center">Sign In</h2>
+class SignIn extends React.Component {
 
-            <Form className="mt-4">
-              <Form.Group>
-                <Form.Label>Enter Your Email</Form.Label>
-                <Form.Control
-                  size="lg"
-                  type="email"
-                  placeholder="Email"
-                  autoComplete="off"
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Enter Your Pasword</Form.Label>
-                <Form.Control
-                  size="lg"
-                  type="password"
-                  placeholder="Password"
-                  autoComplete="off"
-                />
-              </Form.Group>
-              <Button variant="primary" size="lg" className="px-5 mb-4">
-                Login
+    constructor(props){
+        super();
+        this.state={email:"", password:""}
+    }
+
+   submitForm=(e)=>{
+       e.preventDefault();
+       this.props.userData(this.state);
+       this.setState({email:"", password:""})
+    
+   }
+   
+   inputChange= (e)=>{
+      let name = e.target.name;
+       this.setState({[name]:e.target.value})
+   }
+
+
+  render() {
+    return (
+      <div className="signin-wrapper mt-5">
+        <Container>
+          <Row>
+            <Col md={{ span: 6, offset: 3 }}>
+              <h2 className="text-center">Sign In</h2>
+
+              <Form className="mt-4" onSubmit={this.submitForm}>
+                <Form.Group>
+                  <Form.Label>Enter Your Email</Form.Label>
+                  <Form.Control
+                    size="lg"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    autoComplete="off"
+                    value={this.state.email}
+                    onChange={this.inputChange}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Enter Your Pasword</Form.Label>
+                  <Form.Control
+                    size="lg"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="off"
+                    value={this.state.password}
+                    onChange={this.inputChange}
+                  />
+                </Form.Group>
+                <Button type="submit" variant="primary" size="lg" className="px-5 mb-4">
+                  Login
+                </Button>
+              </Form>
+
+              <Button variant="primary" size="lg" className="px-5 mr-3">
+                Google
               </Button>
-            </Form>
-
-            <Button variant="primary" size="lg" className="px-5 mr-3">
-              Google
-            </Button>
-            <Button variant="primary" size="lg" className="px-5">
-              Facbook
-            </Button>
-            <Link className="d-block mt-4" to="/signup">
-              Don't Have any account
-            </Link>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+              <Button variant="primary" size="lg" className="px-5">
+                Facbook
+              </Button>
+              <Link className="d-block mt-4" to="/signup">
+                Don't Have any account
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
